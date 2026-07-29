66ae3b81381530bfe71bd9f162160f18648d5991
Nero <134662565+1101-Grills-Oren@users.noreply.github.com>
2026-07-29 10:41:50 -0700

    Update Check.cs

--- v2/checks/Check.cs
+++ v2/checks/Check.cs
@@ -2,22 +2,25 @@ namespace agame.checks;
 class Check{
 	public string name;
 	public int category;
-  public bool obtained=false;
-  public Action? onObtained=null;
-  public Check(string name, int category){
-    this.name=name;
-    this.category=category;
-  }
-  public Check(string name, int category, Action onObtained){
-    this.name=name;
-    this.category=category;
-    this.onObtained=onObtained;
-  }
-  public void obtain(){
-    if(obtained==false){
-      obtained=true;
-      if(onObtained!=null)
-        onObtained();
-    }
-  }
+	public string id;
+	public bool obtained=false;
+	public Action? onObtained=null;
+	public Check(string name,string id, int category){
+		this.name=name;
+		this.id=id;
+		this.category=category;
+	}
+	public Check(string name, string id, int category, Action onObtained){
+		this.id=id;
+		this.name=name;
+		this.category=category;
+		this.onObtained=onObtained;
+	}
+	public void obtain(){
+		if(obtained==false){
+			obtained=true;
+			if(onObtained!=null)
+				onObtained();
+		}
+	}
 }
