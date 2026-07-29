b30a10be675938d5ee33d2136d520c9300317733
Nero <134662565+1101-Grills-Oren@users.noreply.github.com>
2026-07-29 10:44:05 -0700

    Update CheckSet.cs

--- v2/checks/CheckSet.cs
+++ v2/checks/CheckSet.cs
@@ -1,4 +1,9 @@
 namespace agame.checks;
 public class CheckSet{
-  
+  public Check[] checks=[];
+  public string[] checkids=[];
+  public CheckSet(){}
+  public void addChecks(params Check[] checks){
+    
+  }
 }
